54843a4ce82f7e7691203c8ab095f5b2087ee625
Tobsi <61752@edu.campusvejle.dk>
2024-08-28 15:35:03 -0500

    long[] ticks is now a list<long>

--- src/RunTest/Program.cs
+++ src/RunTest/Program.cs
@@ -9,13 +9,15 @@ namespace RunTest
         static void Main(string[] args)
         {
             Recursion rec = new();
+            string testName = "";
             // Arrange:
             int runs = 4;                                          // Number of measurements
-            long[] ticks = new long[runs];                          // Array to hold the measurements
+            List<long> ticks = new();                          // Array to hold the measurements
             int elements = 10000000;                                // Number of elements
 
             // Generate data:
             for (int i = 0; i < runs; i++)                          // Loop for each run
+            //for (int i = 10; i < 1000000000; i *= 10)
             {
                 Random generator = new Random();                    // Random number generator
                 int[] array = new int[elements];                    // Array to hold the generated numbers
@@ -27,11 +29,13 @@ namespace RunTest
                 Console.WriteLine($"\n=== Test : {i.ToString()}\n");
 
                 // Measure:
+                testName = $"Fibonacci Seq (20)";                 // The name of the file
+                //testName = $"Fibonacci Sequence (20) [ {i} ]";                 // The name of the file
                 Stopwatch stopwatch = new Stopwatch();              // Create stop watch
                 stopwatch.Start();                                  // Start measurement
                 //Array.Sort(array);                                  // PERFORM ALGORITHM
-                //rec.FibonacciSequence(10);
-                rec.FactorialSequence(100);
+                rec.FibonacciSequence(20);
+                //rec.FactorialSequence(100);
                 stopwatch.Stop();                                   // Stop measurement
 
                 // Measurement presentation:
@@ -40,7 +44,7 @@ namespace RunTest
                 //TimeSpan interval = TimeSpan.FromTicks(measuredTicks);
                 //string intervalString = interval.ToString();
 
-                ticks[i] = measuredTicks;                           // Save measurements
+                ticks.Add(measuredTicks);                           // Save measurements
                 //string runResult = $"Run {i + 1}\t: {measuredTicks} : {intervalString}"; // Textual measurement
                 //Console.WriteLine(runResult);                       // Print mesurement
             }
@@ -65,8 +69,8 @@ namespace RunTest
 
             // TODO: Open the file instead of creating a new file everytime 
             // Write data to file (WILL OVERWRITE THE EXISTING FILE)
-            SaveToFile("test", ticks, "csv");
-            SaveToFile("test", savedResults, "txt");
+            SaveToFile(testName, ticks, "csv");
+            SaveToFile(testName, savedResults, "txt");
 
             // TODO: Write avg for ticks and time span. Make sure it lines up with the rest of the table
             Console.WriteLine($"Avg: {ticks.Average()}");           // Print average of measurements
@@ -83,7 +87,7 @@ namespace RunTest
         private static void SaveToFile<T>(string filename, IEnumerable<T> data, string format)
         {
             // I'm adding a timestamp so it doesn't overwrite or write more data to a file by accident
-            filename = filename + $" {(DateTime.Now.ToString()).Replace(':', '.')}"; // You can't use : in filenames
+            filename = filename + $" {(DateTime.Now.ToString()).Replace(':', ';')}"; // You can't use : in filenames
 
             // Makes the filepath before checking the format
             string file = @$".\{filename}.{format}";
