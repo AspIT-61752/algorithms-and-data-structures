38ff7e8ce1ec3ed80ec10e6aff547e056086379a
Tobsi <61752@edu.campusvejle.dk>
2024-08-31 13:20:58 -0500

    SumOf should be done, time to look into some Excel magic

--- src/Lib/TimeSpaceComplexity.cs
+++ src/Lib/TimeSpaceComplexity.cs
@@ -1,7 +1,29 @@
-﻿namespace Lib
+﻿using System.Numerics;
+using System.Text.RegularExpressions;
+
+namespace Lib
 {
     public class TimeSpaceComplexity
     {
+        // - Calculate O from the pseudocode
+        // - Verify with concrete code
+        // - make a visual graph of the measurements
+        // - conclude if your calculated time complexity matches your measurements
 
+        // Calculate O from the pseudocode:
+        //      O(n)
+        // Verify with concrete code
+        // make a visual graph of the measurements
+        // conclude if your calculated time complexity matches your measurements
+        public BigInteger SumOf(int[] arr)
+        {
+            BigInteger total = 0;
+            foreach (var numb in arr)
+            {
+                total += numb;
+            }
+
+            return total;
+        }
     }
 }
--- src/RunTest/Program.cs
+++ src/RunTest/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Lib;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.Numerics;
 
 namespace RunTest
 {
@@ -15,20 +16,16 @@ namespace RunTest
             // Arrange:
             int runs = 1;                                          // Number of measurements
             List<long> ticks = new();                          // Array to hold the measurements
+            List<string> runCount = new();
 
             // Generate data:
             //for (int i = 0; i < runs; i++)                          // Loop for each run
-            for (int i = 10; i < 1000000000; i *= 10)               // 10, 100, 1000, etc.
+            for (int i = 1; i < 1000000000; i *= 10)               // 10, 100, 1000, etc.
             {
 
                 WorstCase testData = new();
 
                 int[] arr = testData.Linear(i);
-
-                for (int j = 0; j < 25; j++)
-                {
-                    Console.WriteLine($"arr[{j}]: {arr[j]}");
-                }
 
                 Console.WriteLine($"\n=== Test : {i.ToString()}\n");
 
@@ -40,17 +37,20 @@ namespace RunTest
                 //Array.Sort(array);                                  // PERFORM ALGORITHM
                 //rec.FibonacciSequence(20);
                 //rec.FactorialSequence(2);
+                //Console.WriteLine($"Sum of arr[{i}]: {tsComplex.SumOf(arr)}");
+                tsComplex.SumOf(arr);
                 stopwatch.Stop();                                   // Stop measurement
 
                 // Save mesurements
                 long measuredTicks = stopwatch.Elapsed.Ticks;
                 ticks.Add(measuredTicks);
+                runCount.Add(i.ToString());
             }
 
             // Displays the result at the end
             List<string> savedResults = new List<string>();
             savedResults.Add($"Test timestamp: {DateTime.Now.ToString()}");
-            string topRow = $"{string.Format(" RUN COUNT").PadLeft(15, '=')} | {string
+            string topRow = $"{string.Format(" RUN").PadLeft(15, '=')} | {string
                 .Format(" TICKS").PadLeft(20, '=')} | {string.Format(" TIME SPAN").PadLeft(16, '=')}";
             savedResults.Add(topRow);
 
@@ -60,14 +60,14 @@ namespace RunTest
             {
                 TimeSpan interval = TimeSpan.FromTicks(ticks[i]);
                 string intervalString = interval.ToString();
-                string runResult = $"Run {i + 1,11} : {ticks[i],20} : {intervalString}"; // Textual measurement
+                string runResult = $"Run {runCount[i],11} : {ticks[i],20} : {intervalString}"; // Textual measurement
                 savedResults.Add(runResult);
                 Console.WriteLine(runResult);
             }
 
             // TODO: Open the file instead of creating a new file everytime 
             // Write data to file (WILL OVERWRITE THE EXISTING FILE)
-            SaveToFile(testName, ticks, "csv");
+            SaveToFile(testName, ticks, "csv", runCount);
             SaveToFile(testName, savedResults, "txt");
 
             // TODO: Write avg for ticks and time span. Make sure it lines up with the rest of the table
@@ -103,6 +103,38 @@ namespace RunTest
                         foreach (var row in data)
                         {
                             stream.WriteLine(row);
+                        }
+                        stream.Close();
+                    }
+                    break;
+                default:
+                    format = "txt";
+                    // TODO: Write to .txt
+                    break;
+            }
+        }
+
+        private static void SaveToFile<T>(string filename, List<T> data, string format, List<string> dataRowTwo)
+        {
+            // I'm adding a timestamp so it doesn't overwrite or write more data to a file by accident
+            filename = filename + $" {(DateTime.Now.ToString()).Replace(':', ';')}"; // You can't use : in filenames
+
+            // Makes the filepath before checking the format
+            string file = @$".\saved data\{filename}.{format}";
+            FileInfo fInfo = new(file);
+            fInfo.Directory.Create();
+
+            switch (format.ToLower())
+            {
+                case "csv":
+                // Fallthrough is intentional
+                // Instead of writing a method or writing the same thing twice, I just removed the break; statement
+                case "txt":
+                    using (var stream = File.CreateText(file))
+                    {
+                        for (int i = 0; i < data.Count(); i++)
+                        {
+                            stream.WriteLine($"{data[i]};{dataRowTwo[i]}");
                         }
                         stream.Close();
                     }
